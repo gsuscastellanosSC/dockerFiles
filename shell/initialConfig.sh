a48f121584da045c173ac266847aef61b39366dd
SC <jesuscastellanospaez@gmail.com>
2021-03-20 00:46:42 -0500

    Update: Dockerfile
    Signed-off-by: SC <jesuscastellanospaez@gmail.com>

--- shell/initialConfig.sh
+++ shell/initialConfig.sh
@@ -1,9 +1,9 @@
-apt-get update && apt-get upgrade -y && apt-get install sudo nano && sudo apt-get update && sudo apt-get upgrade -y;
-sudo apt-get install -y  zsh git-core git-flow;
+apt-get update && apt-get upgrade -y && apt-get install sudo nano && apt-get update && apt-get upgrade -y;
+apt-get install -y  zsh git-core git-flow wget;
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh;
 chsh -s `which zsh`;
-sudo apt-get install fonts-powerline;
-cd ~/.oh-my-zsh/custom/plugins; 
+apt-get install -y fonts-powerline;
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
-sudo apt update && sudo apt upgrade -y;
+cd ~/.oh-my-zsh/custom/plugins;
+apt update && sudo apt upgrade -y;
