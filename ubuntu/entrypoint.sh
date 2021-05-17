ad6ab663cf977bf07a3c22b5a234adc76da33bae
Sc <jesuscastellanospaez@gmail.com>
2021-05-17 02:23:01 -0500

    add: .ssh

--- ubuntu/entrypoint.sh
+++ ubuntu/entrypoint.sh
@@ -5,30 +5,31 @@ echo "######################";
 echo "### entrypoint.sh  ###";
 echo "######################";
 
-# Funciones:
-echo "#################";
-echo "### Funciones ###";
-echo "#################";
-    docker() {
-        chmod 500 /var/run/docker.sock;
-    }
-    ssh(){
-        echo "#################";
-        echo "### Start ssh ###";
-        echo "#################";
-        /etc/init.d/ssh start
-    }
-    entripoint() {
-        echo "##################";
-        echo "### Entripoint ###";
-        echo "##################";
-        tail -f /dev/null
-    }
-    
-# Ejecución
-echo "#################";
-echo "### Ejecución ###";
-echo "#################";
-    docker;
-    ssh;
-    entripoint;
+    echo "#################";
+    echo "### Funciones ###";
+    echo "#################";
+        docker() {
+            echo "##############";
+            echo "### docker ###";
+            echo "##############";
+                chmod 500 /var/run/docker.sock;
+        }
+        ssh(){
+            echo "#################";
+            echo "### Start ssh ###";
+            echo "#################";
+                /etc/init.d/ssh start
+        }
+        entripoint() {
+            echo "##################";
+            echo "### Entripoint ###";
+            echo "##################";
+                tail -f /dev/null
+        }
+        
+    echo "#################";
+    echo "### Ejecución ###";
+    echo "#################";
+        docker;
+        ssh;
+        entripoint;
