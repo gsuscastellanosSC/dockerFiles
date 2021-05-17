51ae44eec6a8ed6ac003f075e5b7bd871e3c90c2
Sc <jesuscastellanospaez@gmail.com>
2021-05-16 23:05:58 -0500

    sh

--- ubuntu/upSSH.sh
+++ ubuntu/upSSH.sh
@@ -5,7 +5,5 @@ echo "###########################";
 echo "### sh /root/upSSH.sh  ###";
 echo "###########################";
 
-service ssh status;
 service ssh start;
 sleep 10s;
-service ssh status;
