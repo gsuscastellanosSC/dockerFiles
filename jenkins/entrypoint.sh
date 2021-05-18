e21f0b283087054c7232ff0a1cf9188aa47f6a0e
Sc <jesuscastellanospaez@gmail.com>
2021-05-17 20:05:24 -0500

    update: Docker files for jenkins

---
+++ jenkins/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+echo "######################";
+echo "### entrypoint.sh  ###";
+echo "######################";
+
+# Funciones
+    # Docker in docker
+    docker() {
+        chmod 500 /var/run/docker.sock;
+    }
+    # Start services
+    startServices(){
+        /etc/init.d/ssh start;
+        /etc/init.d/jenkins start;
+    }
+    # Entripoint
+    entripoint() {
+        docker;
+        startServices;
+        tail -f /dev/null
+    }
+        
+# Ejecución
+  entripoint;
